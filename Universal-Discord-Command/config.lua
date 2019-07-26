c91d1995deb370ba8e9c19774a8e21752e31c717
Matrix Solutions <41305413+MatrixSolutionsGithub@users.noreply.github.com>
2019-07-26 16:54:08 -0400

    Add files via upload

---
+++ Universal-Discord-Command/config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config.webHookURL = "Paste Webhook URL Here" -- Discord Webhook URL
+
+Config.color = "16711710"  -- Discord Embed Color (Default: 16711710 (red))
+						   -- Red: 16711710
+						   -- Green: 1754112
+						   -- Blue: 15615
+						   -- Yellow: 16770560
+						   -- Orange: 16754176
+						   -- White: 16777215
+						   -- Black: 0
+						   -- Pink: 16711901
+						   -- Purple: 9765119
+
+Config.title = "**Bug Report:**"  -- Discord Embed Title (Defualt: **Bug Report:**)
+
+Config.command = "bug"  -- Chat Command (Default: bug)
+
+Config.errorchat = "^1 Please Define The Bug."  -- Response Message When /bug is typed without corresponding message and does nto send to Discord.
+
+Config.successchat = "^4 Bug Report Submitted To Development Team."  -- Response Message When /bug is typed with corresponding message and sends to Discord.
